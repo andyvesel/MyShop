72ef311e8a0ce9f15f172f26daf7b2dff4f9bd12
Andrey <andyvesel@ya.ru>
2014-12-19 10:54:32 +0300

    Update colorpicker

--- public/scripts.js
+++
@@ -1,3 +0,0 @@
-$('select[name="colorpicker"]').simplecolorpicker();
-$('select[name="colorpicker"]').simplecolorpicker('selectColor', '#7bd148');
-$('select[name="colorpicker"]').simplecolorpicker('destroy');
