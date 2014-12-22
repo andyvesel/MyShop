02c96f4757e222263e65ae669b27caef628d613f
Andrey <andyvesel@ya.ru>
2014-12-22 12:46:37 +0300

    Add hash validation

--- app.rb
+++ app.rb
@@ -26,10 +26,16 @@ post '/visit' do
   @your_barber  = params[:your_barber]
   @color        = params[:color]
 
-  if @username == ''
-    @error = 'Введите имя'
-    return erb :visit
-  end
+ hh = { :username => 'Введите имя', 
+        :phone => 'Введите телефон', 
+        :time => 'Введите дату и время', 
+        :your_barber => 'Укажите парикмахера', }
 
+  hh.each do |key, value|
+    if params[key] == ''
+      @error = hh[key]
+      return erb :visit
+    end
+  end
   erb "Ок, #{@username}. Your barber is #{@your_barber}. We'll be waiting for you at #{@time} and call you at number #{@phone}. Color: #{@color}"
 end
